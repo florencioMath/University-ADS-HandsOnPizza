fa5bc0048c82086236a6d7a6e45464b848f3f44d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-06-20 20:45:18 -0300

    feat: Add do menu contendo o Pizza, Clientes e Fornecedores.

---
+++ app/src/main/java/br/com/mfds/ads_handsonworkvi_handsonpizza/MenuFragment.java
@@ -0,0 +1,51 @@
+package br.com.mfds.ads_handsonworkvi_handsonpizza;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class MenuFragment extends Fragment {
+
+    public MenuFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+    }
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_pizza:
+                Toast.makeText(getActivity(), "Menu Pizza", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_clientes:
+                Toast.makeText(getActivity(), "Menu Clientes", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_fornecedores:
+                Toast.makeText(getActivity(), "Menu Fornecedores", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
